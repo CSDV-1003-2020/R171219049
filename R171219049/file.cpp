b577b609f2055c985026f8a4e2633aeee30c30f6
kartikeya532001 <kartikeya532001@gmail.com>
2020-04-08 15:29:36 +0530

    1

--- R171219049/file.cpp
+++ R171219049/file.cpp
@@ -2,20 +2,6 @@
 #include<conio.h>
 void main()
 {
-<<<<<<< HEAD
-k=i+j;
-
-cout<<"sum:"<<k;
-getch();
-=======
-	int i, j,k;
-	cout<<"enter the numbers:";
-	cin>>i>>j;
-	
-<<<<<<< HEAD
-	
-=======
->>>>>>> test
-
->>>>>>> test
+	int i,j,k;
 }
+
